a1f6454b7146fc165bf0cbe320dbb84476b582f2
Alfonso <vivancos@outlook.es>
2019-10-02 16:17:42 +0200

    Create hcaptcha.php
    Empty skeleton of the plugin

---
+++ hcaptcha.php
@@ -0,0 +1,49 @@
+<?php
+
+// Disallow direct access to this file for security reasons
+if(!defined("IN_MYBB"))
+{
+	die("Direct initialization of this file is not allowed.");
+}
+
+
+function myplugin_info()
+{
+	return array(
+		"name"			=> "",
+		"description"	=> "",
+		"website"		=> "",
+		"author"		=> "",
+		"authorsite"	=> "",
+		"version"		=> "1.0",
+		"guid" 			=> "",
+		"codename"		=> "",
+		"compatibility" => "*"
+	);
+}
+
+function myplugin_install()
+{
+
+}
+
+function myplugin_is_installed()
+{
+
+}
+
+function myplugin_uninstall()
+{
+
+}
+
+function myplugin_activate()
+{
+
+}
+
+function myplugin_deactivate()
+{
+
+}
+?>
